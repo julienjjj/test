d52cc253ace75aa15a362ea42f8b1f92635f167d
julienjjj <106631578+julienjjj@users.noreply.github.com>
2022-06-22 12:35:01 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+
+// var yaakovGreeter = {};
+// yaakovGreeter.name = "Yaakov";
+// yaakovGreeter.sayHello = function () {
+// 	console.log("Hello:  " + yaakovGreeter.name);
+// }
+
+// var johnGreeter= {};
+// johnGreeter.name = "John";
+// johnGreeter.sayHi = function (){
+// 	console.log("Hi:  " + johnGreeter.name);
+// }
+
+yaakovGreeter.sayHello();
+johnGreeter.sayHi();
+
+// document.write("this is a test");
+
+// sayHello();
+// sayHi();
+
+
+// var name = "Yaakov";
+// function sayHello () {
+// 	console.log("Hello" + " " +name);
+// }
+
+// sayHello();
+
+// declaring a function named greet()
+
+
+// function greet() {
+//   console.log("Hello there");
+// }
+
+// greet();
